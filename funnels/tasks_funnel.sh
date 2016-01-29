6c33bf03ada79c4d85cf633a6f820db8ceac020c
ToxaZ <az@dreamindustries.co>
2016-01-29 16:26:39 +0300

    refactoring, correct output

---
+++ funnels/tasks_funnel.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+python funnel_calc.py \
+-s -t month web_users_uk registration_web move_to_zvooq_plus payture_gate_shown payture_gate_purchase_clicked subscription # ZA-418.2 general funnel 
